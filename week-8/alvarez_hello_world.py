79f3670b3bd5a3c9dccdbeaa91e038e5aa90e4a3
Karina <alvarez-webdev@gmail.com>
2020-12-02 16:06:35 -0500

    commit

---
+++ week-8/alvarez_hello_world.py
@@ -0,0 +1,11 @@
+"""
+============================================
+; Title:  alvarez_hello_world.py
+; Author: Professor Krasso
+; Date:   2 December 2020
+; Modified by: Karina Alvarez
+; Description: First python file
+===========================================
+"""
+
+print('Hello World')
